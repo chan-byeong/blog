4632569f6c1301c7fcd7d5d20d3edecef33cebc5
byeong <qudcks4885@gmail.com>
2026-01-08 17:17:32 +0900

    fix: filter-item component to use controlled component

--- apps/blog/src/components/side-bar/filter-item.tsx
+++ apps/blog/src/components/side-bar/filter-item.tsx
@@ -1,20 +1,49 @@
-'use client'
+'use client';
 
-import { Checkbox } from '@/components/ui/checkbox'
+import { Checkbox } from '@/components/ui/checkbox';
+import { useState } from 'react';
 
 interface FilterItemProps extends React.ComponentPropsWithRef<typeof Checkbox> {
-  label: string
-  postsCount: number
+  label: string;
+  postsCount: number;
 }
 
-export const FilterItem = ({ label, postsCount, ...props }: FilterItemProps) => {
+export const FilterItem = ({
+  label,
+  postsCount,
+  checked,
+  defaultChecked,
+  onCheckedChange,
+  ...props
+}: FilterItemProps) => {
   // TODO: 컨텍스트로 체크박스 상태 관리 -> 체크 여부에 따라 게시글 필터링
+  const [isChecked, setIsChecked] = useState(
+    defaultChecked || checked || false
+  );
+
+  const handleCheckedChange = (value: boolean) => {
+    setIsChecked(value);
+    onCheckedChange?.(value);
+  };
+
+  const checkState = checked !== undefined ? checked : isChecked;
 
   return (
-    <div className='flex items-center gap-2'>
-      <Checkbox {...props} />
-      <span className='text-sm font-medium text-primary/80'>{label}</span>
-      <span className='text-sm font-medium text-primary/80'>{postsCount}</span>
+    <div
+      className='flex items-center gap-2 group'
+      data-state={checkState ? 'checked' : 'unchecked'}
+    >
+      <Checkbox
+        checked={checkState}
+        onCheckedChange={handleCheckedChange}
+        {...props}
+      />
+      <span className='text-sm font-medium text-primary/80 transition-colors group-data-[state=checked]:text-primary group-data-[state=checked]:font-semibold'>
+        {label}
+      </span>
+      <span className='text-xs font-medium text-primary/60 transition-colors group-data-[state=checked]:text-primary group-data-[state=checked]:font-semibold'>
+        ({postsCount})
+      </span>
     </div>
-  )
-}
+  );
+};
