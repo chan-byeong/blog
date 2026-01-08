c6eb2ab39f9e878f544383e5d309258f3cf010ad
byeong <qudcks4885@gmail.com>
2026-01-08 17:16:27 +0900

    feat: checkbox common component with radix-ui

---
+++ apps/blog/src/components/ui/checkbox.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { cn } from '@/lib/utils';
+
+interface CheckboxProps
+  extends React.ComponentPropsWithRef<typeof CheckboxPrimitive.Root> {
+  className?: string;
+}
+
+const Checkbox = ({ className, ...props }: CheckboxProps) => (
+  <CheckboxPrimitive.Root
+    className={cn(
+      'peer h-3.5 w-3.5 shrink-0 rounded-[3px] border border-zinc-300 transition-all focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-400 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-zinc-900 dark:border-zinc-700 ',
+      className
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn('flex items-center justify-center bg-transparent')}
+    >
+      <i className='hn hn-check text-xs'></i>
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+);
+
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
