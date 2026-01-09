9b356d729bff973abea3de8d4b1a9fd9a5d3e708
byeong <qudcks4885@gmail.com>
2026-01-09 18:11:36 +0900

    feat: side-bar에 post state 연결

--- apps/blog/src/components/side-bar/index.tsx
+++ apps/blog/src/components/side-bar/index.tsx
@@ -1,11 +1,21 @@
-import { FilterGroup } from './filter-group'
-import { Header } from './header'
+'use client';
+
+import { useShallow } from 'zustand/react/shallow';
+import { usePostStore } from '@/providers/post-provider';
+import { FilterGroup } from './filter-group';
+import { Header } from './header';
 
 export const SideBar = () => {
+  const { postCount, postCountByTags } = usePostStore(
+    useShallow((store) => ({
+      postCount: store.postCount,
+      postCountByTags: store.postCountByTags,
+    }))
+  );
   return (
     <div className='sticky top-20 grid grid-cols-subgrid grid-rows-subgrid col-span-5 row-span-2 gap-y-14 self-start'>
-      <Header title='Blog' totalPosts={0} />
-      <FilterGroup />
+      <Header title='Blog' totalPosts={postCount} />
+      <FilterGroup filterItems={postCountByTags} />
     </div>
-  )
-}
+  );
+};
