8641bc2b2a0935f9e616b38503d922ca4fae49da
byeong <qudcks4885@gmail.com>
2026-01-12 14:51:20 +0900

    style: set i tag's display inline-block

--- apps/blog/src/components/side-bar/trigger-button.tsx
+++ apps/blog/src/components/side-bar/trigger-button.tsx
@@ -24,14 +24,14 @@ export const TriggerButton = ({
 
       <div>
         {/* 라이트모드: 닫힘, 열림 아이콘 */}
-        <i className='dark:hidden group-data-[state=open]:hidden hn hn-folder-solid' />
-        <i className='hidden dark:hidden group-data-[state=open]:block hn hn-folder-open-solid' />
+        <i className='inline-block dark:hidden group-data-[state=open]:hidden hn hn-folder-solid' />
+        <i className='hidden dark:hidden group-data-[state=open]:inline-block hn hn-folder-open-solid' />
 
         {/* 다크모드: 닫힘, 열림 아이콘 */}
-        <i className='hidden dark:block dark:group-data-[state=open]:hidden hn hn-folder' />
-        <i className='hidden dark:group-data-[state=open]:block hn hn-folder-open' />
+        <i className='hidden dark:inline-block dark:group-data-[state=open]:hidden hn hn-folder' />
+        <i className='hidden dark:group-data-[state=open]:inline-block hn hn-folder-open' />
       </div>
-      <span className='text-sm font-medium text-primary/80 group-data-[state=open]:font-semibold group-data-[state=open]:text-primary'>
+      <span className='text-[15px] font-medium text-primary/80 group-data-[state=open]:font-semibold group-data-[state=open]:text-primary'>
         {children}
       </span>
     </button>
