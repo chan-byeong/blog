887f02bbf8927e65383788e297bd9da4e3757b3c
byeong <qudcks4885@gmail.com>
2026-01-12 14:50:25 +0900

    fix: extends table header component's props

--- apps/blog/src/components/ui/table-header.tsx
+++ apps/blog/src/components/ui/table-header.tsx
@@ -1,17 +1,22 @@
 import { cn } from '@/lib/utils';
 
-interface TableHeaderProps {
+interface TableHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
 }
 
-export const TableHeader = ({ children, className }: TableHeaderProps) => {
+export const TableHeader = ({
+  children,
+  className,
+  ...props
+}: TableHeaderProps) => {
   return (
     <div
       className={cn(
         'grid grid-cols-subgrid items-center border-b-[0.5px] pb-1.5 border-border bg-transparent self-start',
         className
       )}
+      {...props}
     >
       {children}
     </div>
