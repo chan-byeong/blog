722a01e0850b211348870a32f5c02c01e76c7eca
byeong <qudcks4885@gmail.com>
2026-01-12 11:25:15 +0900

    fix: footer 반응형 크기 조절

--- apps/blog/src/components/footer/index.tsx
+++ apps/blog/src/components/footer/index.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export const Footer = () => {
   return (
-    <footer className='grid grid-cols-subgrid col-span-full border-t-[0.5px] border-border py-3 text-primary/70'>
-      <div className='grid-cols-subgrid col-start-1 col-span-4 text-xs'>
+    <footer className='grid grid-cols-subgrid col-span-full border-t-[0.5px] border-boder py-3 text-primary/70'>
+      <div className='col-start-1 col-span-3 sm:col-start-1 sm:col-span-4 md:col-start-1 md:col-span-5 text-xs'>
         <span>© {new Date().getFullYear()} BYEOUNG.DEV</span>
       </div>
-      <div className='grid-cols-subgrid col-start-20 col-span-5 text-xs flex justify-end items-center gap-2'>
+      <div className='col-start-5 col-span-4 sm:col-start-13 sm:col-span-4 md:col-start-20 md:col-span-5 text-xs flex justify-end items-center gap-2'>
         <Link href='/sitemap.xml' className='flex items-center gap-1'>
           <i className='hn hn-flag-solid inline-block' />
           SITEMAP
