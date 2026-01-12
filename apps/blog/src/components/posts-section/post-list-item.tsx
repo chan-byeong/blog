c85c8fee7aa1f0ad1ea73560b7a5995b2061837a
byeong <qudcks4885@gmail.com>
2026-01-12 14:53:38 +0900

    style: main post list responsive layout

--- apps/blog/src/components/posts-section/post-list-item.tsx
+++ apps/blog/src/components/posts-section/post-list-item.tsx
@@ -11,18 +11,18 @@ import { Tag } from '../ui/tag';
 
 export const PostListItem = ({ post }: PostListItemProps) => {
   return (
-    <li className='grid grid-cols-subgrid col-span-full items-center border-b-[0.5px] border-border'>
+    <li className='grid grid-cols-subgrid col-span-full items-center border-b-[0.5px] border-border/50'>
       <Link
         href={`/posts/${post.slug}`}
         className='grid grid-cols-subgrid col-span-full items-center py-2.5 px-1 self-start hover:bg-accent'
       >
-        <span className='col-start-1 col-span-3 text-sm font-semibold text-primary'>
+        <span className='col-start-1 col-span-2 sm:col-span-3 text-xs font-semibold text-primary'>
           {post.date}
         </span>
-        <span className='col-start-4 col-span-13 text-md font-bold text-primary'>
+        <span className='col-start-3 col-span-4 sm:col-start-4 sm:col-span-10 md:col-span-13 text-md font-bold text-primary'>
           {post.title}
         </span>
-        <span className='col-start-17 col-span-3 text-sm font-semibold text-primary truncate'>
+        <span className='col-start-7 col-span-2 sm:col-start-14 sm:col-span-3 md:col-start-17 md:col-span-3 text-sm font-semibold text-primary truncate'>
           {post.tags?.map((tag) => (
             <Tag key={tag} label={`# ${tag}`} />
           ))}
