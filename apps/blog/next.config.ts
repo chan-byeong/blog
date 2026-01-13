3c40607a36d36006cb35c16acc4fc2d932778800
byeong <qudcks4885@gmail.com>
2026-01-13 21:22:08 +0900

    feat: Add redirects for company subdomain in Next.js configuration

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:company',
+        has: [
+          {
+            type: 'host',
+            value: 'resume.byeoung.dev',
+          },
+        ],
+        destination:
+          'https://byeoung.dev/?utm_source=resume&utm_medium=subdomain&utm_campaign=:company',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
