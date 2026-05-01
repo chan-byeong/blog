30d61f7e038a471b68f2a6ecbc8c86b89f36f007
byeong <qudcks4885@gmail.com>
2026-05-01 18:50:56 +0900

    sitemap 내에 `getAllPostSlug` 제거

--- apps/blog/src/app/sitemap.ts
+++ apps/blog/src/app/sitemap.ts
@@ -1,8 +1,6 @@
-import { getAllPosts } from '@/lib/posts';
 import type { MetadataRoute } from 'next';
 
 export default async function sitemap() {
-  const posts = await getAllPosts();
   const baseUrl = 'https://byeoung.dev';
 
   const staticPages: MetadataRoute.Sitemap = [
@@ -14,12 +12,5 @@ export default async function sitemap() {
     },
   ];
 
-  const postPages: MetadataRoute.Sitemap = posts.map((post) => ({
-    url: `${baseUrl}/posts/${post.slug}`,
-    lastModified: new Date(post.date),
-    changeFrequency: 'monthly',
-    priority: 0.8,
-  }));
-
-  return [...staticPages, ...postPages];
+  return [...staticPages];
 }
